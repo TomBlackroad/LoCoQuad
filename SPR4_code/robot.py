453ded6c166bcaa7b1dffb29e9fb3492bfa8f751
TomBlackroad <34742626+TomBlackroad@users.noreply.github.com>
2019-07-02 20:54:30 +0200

    actuator assignation

--- SPR4_code/robot.py
+++ SPR4_code/robot.py
@@ -16,8 +16,8 @@ class Robot(object):
 		(self.actuators, self.sensors) = utils.file2bot(file, mbl_bots.BOTH)
 		self.pwm = PCA9685(0x40, debug=False)
 		self.pwm.setPWMFreq(50)
-		names_acc = [actuators[i].name for i in len(actuators)]
-		names_sen = [sensors[i].name for i in len(sensors)]
+		names_acc = [self.actuators[i].name for i in len(actuators)]
+		names_sen = [self.sensors[i].name for i in len(sensors)]
 		idx_acc = [i for i in len(actuators)]
 		idx_sen = [i for i in len(sensors)]
 		self.acc_dic = utils.genDictionary(name_acc, idx_acc)
