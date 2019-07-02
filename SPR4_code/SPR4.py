55a8ab4cf52617e08778684248f4901a13bc2a9f
TomBlackroad <34742626+TomBlackroad@users.noreply.github.com>
2019-07-02 13:49:09 +0200

    Identation issues

--- SPR4_code/SPR4.py
+++ SPR4_code/SPR4.py
@@ -11,29 +11,113 @@ class SPR4(Robot):
   """SPR4 spirit"""
   def __init__(self):
     super(SPR4, self).__init__("SPR4.botfile.txt")
-
+    self.home = 90
     self.arise(0.5)
 
 
   def arise(self, behaviour):
-    """
-    """
+    start_time = time.time()
+    while ((time.time()-start_time)<10):
+      self.walkFront(100, 0)
+    start_time = time.time()
+    while True:
+      self.stand()
+    
+
+  def stand(self):
+    logging.debug("SPR4 is standing")
+    super(SPR4,self).moveAcc("FSR", self.home)
+    super(SPR4,self).moveAcc("FSL", self.home)
+    super(SPR4,self).moveAcc("BSR", self.home)
+    super(SPR4,self).moveAcc("BSL", self.home)
+    super(SPR4,self).moveAcc("FLR", self.home)
+    super(SPR4,self).moveAcc("FLL", self.home)
+    super(SPR4,self).moveAcc("BLR", self.home)
+    super(SPR4,self).moveAcc("BLL", self.home)  
 
   def walkFront(self, speed, rand):
-    """
-    """
-  def walkBack(self, speed, rand):
-    """
-    """
-  def walkRight(self, speed, rand):
-    """
-    """
-  def walkLeft(self, speed, rand):
-    """
-    """
+    ms = utils.getDelay(speed)
+    logging.debug("SPR4 is Walking forward")
 
+    super(SPR4,self).moveAcc("RLL", self.home + 50)
+    super(SPR4,self).moveAcc("FRL", self.home + 30)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("FRS", self.home - 45)
+    super(SPR4,self).moveAcc("RRS", self.home + 60)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("FRL", self.home + 80)
+    super(SPR4,self).moveAcc("RLL", self.home + 60)
+    super(SPR4,self).moveAcc("FRL", self.home + 80)
+    super(SPR4,self).moveAcc("RLL", self.home + 50)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("RLS", self.home + 30)
+    super(SPR4,self).moveAcc("FRS", self.home + 20)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("RLL", self.home + 60)
+    super(SPR4,self).moveAcc("FLL", self.home + 80)
+    super(SPR4,self).moveAcc("RLL", self.home - 20)
+    super(SPR4,self).moveAcc("FLL", self.home - 40)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("FLS", self.home + 60)
+    super(SPR4,self).moveAcc("RLS", self.home - 50)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("FLL", self.home - 70)
+    super(SPR4,self).moveAcc("RRL", self.home - 70)
+    super(SPR4,self).moveAcc("FLL", self.home - 40)
+    super(SPR4,self).moveAcc("RRL", self.home - 40)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("RRS", self.home - 20)
+    super(SPR4,self).moveAcc("FLS", self.home)
+    time.sleep(ms)
+    super(SPR4,self).moveAcc("RRL", self.home - 50)
+    super(SPR4,self).moveAcc("FLL", self.home - 70)
+    time.sleep(ms)
+
+
+#  def walkBack(self, speed, rand):
+#    """
+#    """
+#  def walkRight(self, speed, rand):
+#    """
+#    """
+#  def walkLeft(self, speed, rand):
+#    """
+#    """
+
+#                                                             #     
+#  ---------------------------------------------------------  #
+#  ---------------------------------------------------------  #
+#  ---------------------------------------------------------  #
+#            _____                    _____                   #
+#           /\    \                  /\    \                  #
+#          /::\    \                /::\    \                 #   
+#         /::::\    \               \:::\    \                #  
+#        /::::::\    \               \:::\    \               #   
+#       /:::/\:::\    \               \:::\    \              #   
+#      /:::/__\:::\    \               \:::\    \             #
+#     /::::\   \:::\    \              /::::\    \            #     
+#    /::::::\   \:::\    \    ____    /::::::\    \           #     
+#   /:::/\:::\   \:::\    \  /\   \  /:::/\:::\    \          #  
+#  /:::/  \:::\   \:::\____\/::\   \/:::/  \:::\____\         #   
+#  \::/    \:::\  /:::/    /\:::\  /:::/    \::/    /         #       
+#   \/____/ \:::\/:::/    /  \:::\/:::/    / \/____/          #       
+#            \::::::/    /    \::::::/    /                   #         
+#             \::::/    /      \::::/____/                    #     
+#             /:::/    /        \:::\    \                    #     
+#            /:::/    /          \:::\    \                   #     
+#           /:::/    /            \:::\    \                  #    
+#          /:::/    /              \:::\____\                 #  
+#          \::/    /                \::/    /                 #  
+#           \/____/                  \/____/                  #    
+#                                                             #  
+#  ---------------------------------------------------------  #
+#  ---------------------------------------------------------  #
+#  ---------------------------------------------------------  #
+#                                                             #   
 
 if __name__=='__main__':
+  SPR4 = SPR4()
+  
 
 """
 if __name__=='__main__':
