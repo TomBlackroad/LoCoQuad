3887be7aa3aba6a956bf0201689e522371baa5e4
TomBlackroad <34742626+TomBlackroad@users.noreply.github.com>
2019-08-25 02:09:38 +0200

    bug_fixed

--- SPR4_code/move.py
+++ SPR4_code/move.py
@@ -1,6 +1,6 @@
 class Movement(object):
     """docstring for Movement"""
     def __init__(self, *args):
-        super(Actuator, self).__init__()
+        super(Movement, self).__init__()
         self.actuator = args[0][0]
         self.pos = int(args[0][1])
