5a6b13e6b2afbdda20d357b013a9f517ac4373fc
TomBlackroad <34742626+TomBlackroad@users.noreply.github.com>
2019-08-29 22:21:24 +0200

    FSM explorer12

--- SPR4_code/IMU.py
+++ SPR4_code/IMU.py
@@ -8,17 +8,17 @@ import mbl_bots
 
 class IMU:
 	#some MPU6050 Registers and their Address
-	PWR_MGMT_1   = 0x6B
-	SMPLRT_DIV   = 0x19
-	CONFIG       = 0x1A
-	GYRO_CONFIG  = 0x1B
-	INT_ENABLE   = 0x38
-	ACCEL_XOUT_H = 0x3B
-	ACCEL_YOUT_H = 0x3D
-	ACCEL_ZOUT_H = 0x3F
-	GYRO_XOUT_H  = 0x43
-	GYRO_YOUT_H  = 0x45
-	GYRO_ZOUT_H  = 0x47
+	# PWR_MGMT_1   = 0x6B
+	# SMPLRT_DIV   = 0x19
+	# CONFIG       = 0x1A
+	# GYRO_CONFIG  = 0x1B
+	# INT_ENABLE   = 0x38
+	# ACCEL_XOUT_H = 0x3B
+	# ACCEL_YOUT_H = 0x3D
+	# ACCEL_ZOUT_H = 0x3F
+	# GYRO_XOUT_H  = 0x43
+	# GYRO_YOUT_H  = 0x45
+	# GYRO_ZOUT_H  = 0x47
 
 	def __init__(self, bus):
 		#bus = smbus.SMBus(1)    # or bus = smbus.SMBus(0) for older version boards
