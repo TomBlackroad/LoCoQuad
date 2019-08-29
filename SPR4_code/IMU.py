5fa4497371c584c428769aa62bd3342d33d21db6
TomBlackroad <34742626+TomBlackroad@users.noreply.github.com>
2019-08-29 22:05:44 +0200

    FSM explorer9

--- SPR4_code/IMU.py
+++ SPR4_code/IMU.py
@@ -60,29 +60,29 @@ class IMU:
 		data = [0,0,0,0,0,0]
 		try:
 	        #Read Accelerometer raw value
-	        acc_x = read_raw_data(ACCEL_XOUT_H)
-	        acc_y = read_raw_data(ACCEL_YOUT_H)
-	        acc_z = read_raw_data(ACCEL_ZOUT_H)
+			acc_x = read_raw_data(ACCEL_XOUT_H)
+			acc_y = read_raw_data(ACCEL_YOUT_H)
+			acc_z = read_raw_data(ACCEL_ZOUT_H)
 
 	        #Read Gyroscope raw value
-	        gyro_x = read_raw_data(GYRO_XOUT_H)
-	        gyro_y = read_raw_data(GYRO_YOUT_H)
-	        gyro_z = read_raw_data(GYRO_ZOUT_H)
+			gyro_x = read_raw_data(GYRO_XOUT_H)
+			gyro_y = read_raw_data(GYRO_YOUT_H)
+			gyro_z = read_raw_data(GYRO_ZOUT_H)
 
 	        #Full scale range +/- 250 degree/C as per sensitivity scale factor
-	        Ax = acc_x/16384.0
-	        Ay = acc_y/16384.0
-	        Az = acc_z/16384.0
+			Ax = acc_x/16384.0
+			Ay = acc_y/16384.0
+			Az = acc_z/16384.0
 
-	        Gx = gyro_x/131.0
-	        Gy = gyro_y/131.0
-	        Gz = gyro_z/131.0
+			Gx = gyro_x/131.0
+			Gy = gyro_y/131.0
+			Gz = gyro_z/131.0
 
-	        data = [Ax,Ay,Az,Gx,Gy,Gz]
-	        return data
+			data = [Ax,Ay,Az,Gx,Gy,Gz]
+			return data
 
 	    except:
-	    	print("Error getting IMU data... something went wrong!!")
-	    	return data
+			print("Error getting IMU data... something went wrong!!")
+			return data
 
-        print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)
+		print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)
